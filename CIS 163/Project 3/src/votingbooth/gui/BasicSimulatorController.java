d608fd927cf035ecd09e7312c414d43de7aad46d
Kyle Flynn <kyle.flynn5798@yahoo.com>
2016-10-12 17:45:00 -0400

    Project 3 Defaults Set. Ready to begin.

---
+++ CIS 163/Project 3/src/votingbooth/gui/BasicSimulatorController.java
@@ -0,0 +1,18 @@
+package votingbooth.gui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by kylef_000 on 10/12/2016.
+ */
+public class BasicSimulatorController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
