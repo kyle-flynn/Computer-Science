320fcaa0d4573268b9ed3103e05d708388b1770b
Kyle Flynn <kyle.flynn5798@yahoo.com>
2016-11-01 15:19:12 -0400

    More Conflicts

--- CIS 163/Project 3/src/votingbooth/BoothLine.java
+++ CIS 163/Project 3/src/votingbooth/BoothLine.java
@@ -30,50 +30,24 @@ public class BoothLine implements ClockListener {
             for (int i = 0; i < Q.size(); i++) {
                 Q.get(i).addTime(1);
                 if (Q.get(i).getTimeSpent() >= Q.get(i).getTolerance()) {
-<<<<<<< HEAD
-                    // fuck this shit im out
                     System.out.println("Voter " + Q.get(i).getVoterID() + ": FUCK THIS");
-                    peoplePissed(Q.remove(i));
-=======
                     Q.get(i).setPissed(true);
                     Q.remove(i);
->>>>>>> origin/master
                 }
             }
         }
-            for (Booth b : booths) {
-                if (!b.inUse()) {
-                    if (Q.get(0).getStatus() == VoterStatus.WAITING_FOR_BOOTH) {
-                        Voter person = Q.remove(0);
-                        b.add(person);
-                        break;
-                    }
-                }
-            }
-
-        }
-
-    /**
-     * Calculate the people that are 'pissed' and leave the Voting Q. Parses through the
-     * ArrayList and ticks the time until it gets to the "tolerance" point. If the tolerance
-     * is exceeded
-     *
-     *
-     * @param person is of type Voter to be used with the ArrayList
-     * @return the ArrayList
-     */
-    public ArrayList<Voter> peoplePissed(Voter person){
-        if (pissed.size() >= 1) {
-            for (int i = 0; i < pissed.size(); i++) {
-                pissed.get(i).addTime(1);
-                if (pissed.get(i).getTimeSpent() >= pissed.get(i).getTolerance()) {
-                    pissed.add(person);
-
+        for (Booth b : booths) {
+            if (!b.inUse()) {
+                if (Q.get(0).getStatus() == VoterStatus.WAITING_FOR_BOOTH) {
+                    Voter person = Q.remove(0);
+                    b.add(person);
+                    break;
                 }
             }
         }
-        return pissed;
     }
+
+
 
     public void setBooths(Booth[] booths) {
         this.booths = new Booth[booths.length];
