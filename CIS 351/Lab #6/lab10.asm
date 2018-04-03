46f1232bfde497e3662dbd5199c8940154b131d3
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-04-03 15:05:19 -0400

    Updated lab #11

--- CIS 351/Lab #6/lab10.asm
+++ CIS 351/Lab #6/lab10.asm
@@ -1,21 +1,17 @@
 .text
-.globl main
+.globl nCk
 
-main:
-	# Initialize the a0 register with a value that we want to see for fibonacci.
-	li $a0, 5
-	li $a1, 4
-	
+nCk:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 
-	jal nCk 	
+	jal nChoosek 	
 
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4	
 	jr $ra
 	
-nCk:
+nChoosek:
 
 	addi $sp, $sp, -16 
 	sw $ra, 0($sp)
@@ -27,34 +23,35 @@ nCk:
 	add $s1, $a1, $0 # new k
 
 	addi $t1, $0, 1
-	beq $s0, $0, return1 # if n = 0, return 1
-	beq $s0, $t1, returnN # if n = 1, return n
-	beq $s0, $s1, return1 # if n = k, return 1 
+	beq $s1, $0, return1 # if n = 0, return 1
+	beq $s1, $t1, returnN # if n = 1, return 1
+	beq $s0, $s1, return1 # if n = k, return N 
 
 	addi $a0, $s0, -1 # n - 1
 	
 	# Do n-1 choose k
-	jal nCk
+	jal nChoosek
 
 	add $s2, $0, $v0 # this is our result of the previous function
 
-	# Do n-1 choose k-1
+	addi $a0, $s0, -1 # n - 1
 	addi $a1, $s1, -1 # k - 1
 
-	jal nCk               
+	# Do n-1 choose k-1
+	jal nChoosek               
 
-	add $v0, $v0, $s2 # This is the result of n-1 choose k-1 and n-1 choose k
+    add $v0, $v0, $s2 # This is the result of n-1 choose k-1 and n-1 choose k
 exit:
-
 	lw $ra, 0($sp)       
 	lw $s0, 4($sp)
 	lw $s1, 8($sp)
-	addi $sp, $sp, 12     
+	lw $s2, 12($sp)
+	addi $sp, $sp, 16
 	jr $ra
 
 return1:
-	li $v0, 1
+	addi $v0, $0, 1
 	j exit
 returnN:     
-	li $v0, $s0
+	add $v0, $s0, $0
 	j exit
---
+++ CIS 351/Lab #6/nCk.java
@@ -0,0 +1,50 @@
+import org.junit.*;
+
+import static edu.gvsu.mipsunit.munit.MUnit.Register.*;
+import static edu.gvsu.mipsunit.munit.MUnit.*;
+
+public class nCk {
+
+    @Before
+    public void before() {
+    set(v0, 1337);
+    }
+
+    /******************************************************************
+     *
+     * Test makes10
+     *
+     *****************************************************************/
+
+    @Test 
+    public void nCk_5_3() {
+    run("nCk", 5, 3);
+    Assert.assertEquals(10, get(v0));
+    }
+
+    @Test 
+    public void nCk_100_4() {
+    run("nCk", 100, 4);
+    Assert.assertEquals(3921225, get(v0));
+    }
+
+	@Test
+	public void nCk_2_1() {
+	run("nCk", 2, 1);
+	Assert.assertEquals(2, get(v0));
+	}
+	
+	@Test
+	public void nCk_2_2() {
+	run("nCk", 2, 2);
+	Assert.assertEquals(1, get(v0));
+	}
+	
+    /******************************************************************
+     *
+     * Write many more tests!  * Test Edge Cases *
+     *
+     *****************************************************************/
+    
+} // end class
+
