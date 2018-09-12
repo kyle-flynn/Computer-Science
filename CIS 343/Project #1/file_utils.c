d93856882b576bfbc0878ddc2f99939dcef0ebbc
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-09-12 14:49:09 -0400

    Added code comments.

--- CIS 343/Project #1/file_utils.c
+++ CIS 343/Project #1/file_utils.c
@@ -4,17 +4,30 @@
 
 #include "file_utils.h"
 
+/**
+ * The following method reads a given filename and loads it into the provided buffer.
+ *
+ * @param {char*} filename - The location of the file.
+ * @param {char**} buffer - The buffer that the file's contents will be dumped to.
+ */
 int read_file(char* filename, char **buffer) {
     FILE* file = fopen(filename, "r");
+
+    // If the file wasn't found, return the designated error code to the caller.
     if (file == NULL) {
         return errno;
     }
 
-    // I prefer this method instead of importing stat.h
+    /*
+     * Instead of using sys/stat.h, stdio.h provides a seek method that does roughly the same thing. I could've made
+     * my life easier and used the provided code, but I wanted to be different and found this all by myself in the C
+     * documentation. Yay me.
+     */
     fseek(file, 0L, SEEK_END);
     size_t size = (size_t) ftell(file);
     fseek(file, 0L, SEEK_SET);
 
+    // De-referencing the buffer and setting the content to the file's contents.
     *buffer = (char*) malloc(size * sizeof(char*));
     fread(*buffer, sizeof(char), size, file);
     fclose(file);
@@ -22,12 +35,22 @@ int read_file(char* filename, char **buffer) {
     return 0;
 }
 
+/**
+ * The following methods writes to a given filename with the provided content buffer and size.
+ *
+ * @param {char*} filename - The location of the file
+ * @param {char*} buffer - The content buffer that will be written to the file
+ * @param {size_t} size - The desired size of the file
+ */
 int write_file(char* filename, char *buffer, unsigned int size) {
     FILE* file = fopen(filename, "w");
+
+    // If the file wasn't found, return the designated error code to the caller.
     if (file == NULL) {
         return errno;
     }
 
+    // Very simply write back to the file and close it. The fprintf function will handle \n's and all original bytes.
     fprintf(file, buffer);
     fclose(file);
 
--- CIS 343/Project #1/reverse.c
+++ CIS 343/Project #1/reverse.c
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include "file_utils.h"
 
+/**
+ * The following helper method reverses a given string into a destination string.
+ *
+ * @param {char*} src - The source string.
+ * @param {char*} dest - The destination, or result that the reversed string will load in to.
+ */
 int reverse(const char* src, char* dest) {
     int size = (int) strlen(src);
+
+    /*
+     * Create two variables, one counts up and the other counts down. Loop through the src string backwards, and loop
+     * through the destination string forwards.
+     */
     for (int i = size, j = 0; i >= 0; i--) {
+
+        // Make sure the null byte isn't the first character in the destination string.
         if (src[i] != '\0') {
             dest[j] = src[i];
             j++;
@@ -13,14 +26,25 @@ int reverse(const char* src, char* dest) {
     return EXIT_SUCCESS;
 }
 
+/**
+ * The main method of the program. Takes an input file to reverse it's contents, and outputs it to another file.
+ *
+ * @param {int} argc - The amount of commandline arguments.
+ * @param {char**} argv - The arguments themselves in a char** representation.
+ */
 int main(int argc, char** argv) {
 
-    if (argc < 2) {
-        fprintf(stderr, "Invalid number of arguments. ");
+    // This program only accepts 2 strings. Make sure the user knows that.
+    if (argc != 3) {
+        fprintf(stderr, "Invalid number of arguments.");
         return EXIT_FAILURE;
     } else {
+
+        // Creating friendly, local variables of our arguments.
         char* read_loc = argv[1];
         char* write_loc = argv[2];
+
+        // Dynamically allocating memory for our buffer, and getting the response code from our read_file method.
         char **buffer = (char**) malloc(sizeof(char**));
         int read_response = read_file(read_loc, buffer);
 
@@ -29,17 +53,17 @@ int main(int argc, char** argv) {
             return read_response;
         }
 
+        // Reversing our string, and getting the response code from our write_file method.
         char* reversed_str = (char*) malloc(sizeof(buffer));
         reverse(*buffer, reversed_str);
-
         int write_response = write_file(write_loc, reversed_str, sizeof(reversed_str));
 
         if (write_response != EXIT_SUCCESS) {
             printf("There was an error writing to the file. Error code " + write_response);
             return write_response;
         }
-
     }
 
+    // If no other exit code, then the program was a success.
     return EXIT_SUCCESS;
 }
