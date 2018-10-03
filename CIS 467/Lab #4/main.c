8f90531478a6cdca7c3c897b6b1dff75ba8720a5
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-10-03 14:59:20 -0400

    Almost done wtih lab

--- CIS 467/Lab #4/main.c
+++ CIS 467/Lab #4/main.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <linux/if_packet.h>
-#include <linux/if_ether.h>
+#include <netpacket/packet.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <net/if.h>
 
 int main(int argc, char** argv) {
+
 	int sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));	
-  if (sockfd < 0) {
+
+	if (sockfd < 0) {
     printf("There was an error creating the socket [%d].\n", sockfd);
 		return -1;
 	}
 
-  struct sockaddr_ll sock_addr;
-	sock_addr.sll_family = AF_PACKET;
-	sock_addr.sll_protocol = htons(ETH_P_ALL);
-	sock_addr.sll_ifindex = if_nametoindex("eth0");
+  struct sockaddr_ll serv_addr, cli_addr;
+	serv_addr.sll_family = AF_PACKET;
+	serv_addr.sll_protocol = htons(ETH_P_ALL);
 
-	int sock_bind = bind(sockfd, (struct sockaddr*)&sock_addr, (socklen_t) sizeof(sock_addr));
+  if (argc < 1) {
+		serv_addr.sll_ifindex = if_nametoindex("eth0");
+	} else {
+		serv_addr.sll_ifindex = if_nametoindex(argv[1]);
+	}
+
+	int sock_bind = bind(sockfd, (struct sockaddr*)&serv_addr, (socklen_t) sizeof(serv_addr));
 
   if (sock_bind < 0) {
 	  printf("There was an error binding the socket to the address and port.\n");
@@ -26,11 +34,23 @@ int main(int argc, char** argv) {
 	}
   
   while (1) {
-    int length = sizeof(sock_addr);
+    int length = sizeof(cli_addr);
 		char message[4098];
-    int res = recvfrom(sockfd, message, 4098, 0, (struct sockaddr*)&sock_addr, &length);
+    int res = recvfrom(sockfd, message, 4098, 0, (struct sockaddr*)&cli_addr, &length);
     if (res < 0) {
       printf("Error receiving packet.\n");
+		} else {
+      
+      if (cli_addr.sll_pkttype != PACKET_OUTGOING) {
+        struct ether_header* pck_headr = (struct ether_header*) message;
+				struct iphdr* ip_headr = (struct iphdr*) (message + sizeof(pck_headr));
+			  printf("\n---------- INCOMING PACKET HEADER ----------\n");
+				printf("Destination: %s\n", pck_headr->ether_shost);
+				printf("Source: %s\n", pck_headr->ether_dhost);
+				printf("Type: %d\n", ntohs(pck_headr->ether_type));
+				printf("\n--------------------------------------------\n");
+			}
+
 		}
 	}
 
