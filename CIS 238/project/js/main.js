99ae62fe0b1f004a93bfbee8d188e2b2fb67e8c1
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-03-16 12:45:59 -0400

    Finished midterm. Needs uploading.

--- CIS 238/project/js/main.js
+++ CIS 238/project/js/main.js
@@ -1,19 +1,22 @@
 window.addEventListener("load", initialize);
 
 function initialize() {
-  document.getElementById("mobile-toggle").addEventListener("click", toggleMobileMenu)
+  document.getElementById("mobile-menu-toggle").addEventListener("click", toggleMobileMenu)
 }
 
 function toggleMobileMenu() {
   let navElements = document.getElementById("mobile-menu").getElementsByTagName("nav");
   if (navElements.length > 0) {
     let navElement = navElements[0];
+    let container = document.getElementById("container");
     if (navElement.classList.contains("hidden")) {
       navElement.classList.remove("hidden");
-      navElement.style.transform = "translateY(0%)";
+      navElement.style.transform = "translateX(0%)";
+      container.style.opacity = "0.4";
     } else {
       navElement.classList.add("hidden");
-      navElement.style.transform = "translateY(-125%)";
+      navElement.style.transform = "translateX(-80vw)";
+      container.style.opacity = "1.0";
     }
   }
 }
