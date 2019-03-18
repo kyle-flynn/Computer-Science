34c47a7622433a21c0fd3544291fc72b1f56b652
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-03-18 14:45:46 -0400

    Framework for menu is done.

--- CIS 238/project/js/main.js
+++ CIS 238/project/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener("load", initialize);
 function initialize() {
   document.getElementById("mobile-menu-toggle").addEventListener("click", toggleMobileMenu);
   document.getElementById("overlay").addEventListener("click", closeMobileMenu);
+  document.getElementById("mobile-overlay").addEventListener("click", closeMobileMenu);
 }
 
 function toggleMobileMenu() {
@@ -21,15 +22,24 @@ function openMobileMenu() {
   let navElements = document.getElementById("mobile-menu").getElementsByTagName("nav");
   let navElement = navElements[0];
   let overlay = document.getElementById("overlay");
+  let mobileOverlay = document.getElementById("mobile-overlay");
   navElement.classList.remove("hidden");
   navElement.style.transform = "translateX(0%)";
-  overlay.style.display = "block";
+  overlay.style.width = "100vw";
+  overlay.style.opacity = "1.0";
+  mobileOverlay.style.width = "100vw";
+  mobileOverlay.style.opacity = "1.0";
 }
 
 function closeMobileMenu() {
   let navElements = document.getElementById("mobile-menu").getElementsByTagName("nav");
   let navElement = navElements[0];
+  let overlay = document.getElementById("overlay");
+  let mobileOverlay = document.getElementById("mobile-overlay");
   navElement.classList.add("hidden");
   navElement.style.transform = "translateX(-80vw)";
-  overlay.style.display = "none";
+  overlay.style.width = "0";
+  overlay.style.opacity = "0";
+  mobileOverlay.style.width = "0";
+  mobileOverlay.style.opacity = "0";
 }
